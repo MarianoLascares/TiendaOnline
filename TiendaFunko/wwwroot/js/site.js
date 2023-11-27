ba6fb3314b0acbe09c3fad82362e2a52a868b56a
Mariano Lascares <mariano.lascares@hotmail.com>
2023-11-27 17:47:01 -0300

    Update CSS site

--- TiendaFunko/wwwroot/js/site.js
+++ TiendaFunko/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const menuToggle = document.querySelector('.menu-toggle');
+const navbarMenu = document.querySelector('.nabvar__menu');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navbarMenu.style.display = navbarMenu.style.display === 'block' ? 'none' : 'block';
+});
